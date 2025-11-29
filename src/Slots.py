c7bee2a73abfaa231f39d7da97a7a53000d696b9
Kuba Bigaj <kbigaj2002@gmail.com>
2025-11-29 14:47:09 +0100

    Corrected the class so it can better interface with the graphx library

---
+++ src/Slots.py
@@ -0,0 +1,44 @@
+import warnings
+
+
+class Slots:
+    """ Stores data about the EON connection slots for use in the graphX library"""
+
+    def __init__(self):
+        self.__slots : list[bool] = [False] * 320 # stores info about the availability of slots
+
+    def is_spectrum_free(self, start:int, length:int) -> bool:
+        """
+        Checks weather the specified slots are free
+        :param start: The first slot of the connection
+        :param length: The length of the connection
+        :return: True if the connection is free, False otherwise
+        """
+        for i in range(start, length):
+            if self.__slots[i]:
+                return False
+        return True
+
+    def reserve_slots(self, start: int, length: int) -> None:
+        """
+        Reserve slots along the connection.
+        :param start: The first slot of the connection
+        :param length: The length of the connection
+        :return: None
+        """
+        if not self.is_spectrum_free(start, length):
+            raise ValueError("Slots not free!") # safety
+        for i in range(start, length):
+            self.__slots[i] = True
+
+    def free_slots(self, start: int, length: int) -> None:
+        """
+        Free used slots along the connection.
+        :param start: The first slot of the connection
+        :param length: The length of the connection
+        :return: None
+        """
+        for i in range(start, length):
+            if not self.__slots[i]:
+                warnings.warn("Freed up an already free slot!", RuntimeWarning) # safety
+            self.__slots[i] = False
