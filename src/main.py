d505151590d292c3d5bde29d8634e0be8fe2c9a0
Kuba Bigaj <kbigaj2002@gmail.com>
2025-11-29 15:19:42 +0100

    Added a showcase of accessing the created data structures in a convenient way in main.py. Also renamed files to be compliant with best practices.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 import networkx as nx
+from slots import Slots
 
-G = nx.Graph()
+G = nx.DiGraph()
 
-G.add_edge("A", "B", weight=4)
-G.add_edge("B", "D", weight=2)
-G.add_edge("A", "C", weight=3)
-G.add_edge("C", "D", weight=4)
+G.add_edge("A", "B", slots = Slots())
+G.add_edge("B", "D", slots = Slots())
+G.add_edge("A", "C", slots = Slots())
+G.add_edge("C", "D", slots = Slots())
 
-print(nx.shortest_path(G, "A", "D", weight="weight"))
+cur_slots : Slots = G["A"]["B"]["slots"]
+
+print(cur_slots.is_spectrum_free(0, 9))
+cur_slots.reserve_slots(0, 9)
+print(cur_slots.is_spectrum_free(0, 9))
+
+for n in G:
+    print(f"Node: {n}, type: {type(n)}")
+
+print("----")
+for n in G:
+    for e in G[n]:
+        print(f"Edge from {n} to {e}: {G[n][e]}, type: {type(G[n][e])}")
