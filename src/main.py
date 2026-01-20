ce8a36a41e57ca957100383a46205ab712e11136
Boris Komarov <boris.komarov@proton.me>
2026-01-20 18:25:42 +0100

    testsuit corrected to match updated version

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import time
-import copy
 import csv
 
 from src.algorithms.first_fit_ng import FirstFitNG
@@ -51,85 +50,78 @@ TESTS = [
 ]
 
 def run_tests(test_case: TestInfo, MOD, output_file="results.csv"):
-    FOLDER_LIMIT = 500
+    req_sets = [100, 200, 300, 400, 500]
+
+    algorythms = [
+        "First Fit No Grooming",
+        "First Fit Static Grooming",
+        "Exact Fit No Grooming",
+        "Exact Fit Static Grooming",
+    ]
+
     net_obj = read_graph_from_file(test_case.net)
     pat_obj = read_routes_from_file(test_case.pat, net_obj)
-    dem_obj = read_connections_from_folder(test_case.dem, 500)
 
-    # Open file in append mode
-    with open(output_file, "a") as f:
-        # Write net, pat, dem info
-        f.write(f"net, {test_case.net}\n")
-        f.write(f"pat, {test_case.pat}\n")
-        f.write(f"dem, {test_case.dem}\n")
+    for i, algorythm in enumerate(algorythms):
+        with open(output_file, "a") as res_file:
+            res_file.write(f"net: {test_case.net}\n")
+            res_file.write(f"dem: {test_case.dem}\n")
+            res_file.write(f"algorithm: {algorythm}\n\n")
 
-        # Dictionary to hold results
-        results = {}
+            res_file.write(f"requests; avg_transceiver_use; avg_dropped_bitrate\n")
 
-        # FirstFitNG
-        first_fit_ng = FirstFitNG(
-            graph       = net_obj, # note: graphs, connections, routes and modulations are read only, no need for a deep copy
-            connections = dem_obj,
-            routes      = pat_obj,
-            modulations = MOD)
-        first_fit_ng.run()
-        results["FirstFitNG"] = first_fit_ng.get_overall_performance()
+            for req_set in req_sets:
+                dem_obj = read_connections_from_folder(test_case.dem, req_set)
 
-        # FirstFitSG
-        first_fit_sg = FirstFitSG(
-            graph       = copy.deepcopy(net_obj),
-            connections = copy.deepcopy(dem_obj),
-            routes      = copy.deepcopy(pat_obj),
-            modulations = MOD)
-        first_fit_sg.run()
-        results["FirstFitSG"] = first_fit_sg.get_overall_performance()
+                result: tuple[float, float]
 
-        # ExactFitNG
-        exact_fit_ng = ExactFitNG(
-            graph       = copy.deepcopy(net_obj),
-            connections = copy.deepcopy(dem_obj),
-            routes      = copy.deepcopy(pat_obj),
-            modulations = MOD)
-        exact_fit_ng.run()
-        results["ExactFitNG"] = exact_fit_ng.get_overall_performance()
+                match i:
+                    case 0:
+                        ffng = FirstFitNG(
+                            graph       = net_obj,
+                            connections = dem_obj,
+                            routes      = pat_obj,
+                            modulations = MOD)
 
-        # ExactFitSG
-        exact_fit_sg = ExactFitSG(
-            graph       = copy.deepcopy(net_obj),
-            connections = copy.deepcopy(dem_obj),
-            routes      = copy.deepcopy(pat_obj),
-            modulations = MOD)
-        exact_fit_sg.run()
-        results["ExactFitSG"] = exact_fit_sg.get_overall_performance()
+                        ffng.run()
 
-        # Write results to file
-        for algo, result in results.items():
-            f.write(f"{algo}, {result}\n")
+                        result = ffng.get_overall_performance()
+                    case 1:
+                        ffsg = FirstFitSG(
+                            graph       = net_obj,
+                            connections = dem_obj,
+                            routes      = pat_obj,
+                            modulations = MOD)
 
-        # Add a blank line between blocks
-        f.write("\n")
+                        ffsg.run()
+
+                        result = ffsg.get_overall_performance()
+                    case 2:
+                        efng = ExactFitNG(
+                            graph       = net_obj,
+                            connections = dem_obj,
+                            routes      = pat_obj,
+                            modulations = MOD)
+
+                        efng.run()
+
+                        result = efng.get_overall_performance()
+                    case 3:
+                        efsg = ExactFitSG(
+                            graph       = net_obj,
+                            connections = dem_obj,
+                            routes      = pat_obj,
+                            modulations = MOD)
+
+                        efsg.run()
+
+                        result = efsg.get_overall_performance()
+                res_file.write(f"{req_set}; {result[0]}; {result[1]}\n")
+            res_file.write(f"\n")
 
 #########################
 # BEGIN                 #
 #########################
-
-net_obj = read_graph_from_file("./assets/POL12/pol12.net")
-pat_obj = read_routes_from_file("./assets/POL12/pol12.pat", net_obj)
-dem_obj = read_connections_from_folder("./assets/POL12/demands_0", -1) # heavy load
-
-# alg = FirstFitSG(
-#     graph       = net_obj,
-#     connections = dem_obj,
-#     routes      = pat_obj,
-#     modulations = MOD)
-
-alg = ExactFitSG(graph=net_obj, connections=dem_obj, routes=pat_obj, modulations=MOD)
-
-alg.run() # disable the debug in the algorithm.py class to avoid too many prints
-
-print(f"Overall performance: {alg.get_overall_performance()}")
-
-exit()
 
 start = time.time()
 
